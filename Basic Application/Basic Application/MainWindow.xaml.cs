1220f3e5b3521bed42c957f1ea198c19210f2ddc
Edward <spottablefiend@gmail.com>
2019-01-31 14:39:06 +0000

    First push with Lukas' stuff

---
+++ Basic Application/Basic Application/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Basic_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            tblMessage.Text = tbMessage.Text;
+            tbMessage.Clear();
+
+            TcpClient client = new TcpClient("localhost", 2302);
+
+            byte[] message = ASCIIEncoding.ASCII.GetBytes(tblMessage.Text);
+
+            client.GetStream().Write(message, 0, message.Length);
+
+            client.Close();
+        }
+    }
+}
---
+++ Basic Application/PingPongServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PingPongServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 2302);
+            listener.Start();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] message = new byte[client.ReceiveBufferSize];
+
+                int bytesRead = stream.Read(message, 0, client.ReceiveBufferSize);
+
+                string dataReceived = Encoding.ASCII.GetString(message, 0, bytesRead);
+
+                Console.WriteLine(dataReceived);
+
+                client.Close();
+            }
+                
+        }
+    }
+}
