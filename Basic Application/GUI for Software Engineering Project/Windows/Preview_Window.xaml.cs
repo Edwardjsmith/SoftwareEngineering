896d2754e99ba282ff5a75b822578a2f6b3b9da7
Lukas Hornischer <lukas-hornischer@t-online.de>
2019-03-11 17:00:36 +0000

    Updated GUI

---
+++ Basic Application/GUI for Software Engineering Project/Windows/Preview_Window.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GUI_for_Software_Engineering_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_for_Software_Engineering_Project.Windows
+{
+    /// <summary>
+    /// Interaction logic for Preview_Window.xaml
+    /// </summary>
+    public partial class Preview_Window : Window
+    {
+        public Preview_Window(AssetData asset)
+        {
+            InitializeComponent();
+
+            imgPreview.Source = asset.ImgSource;
+
+            lblPreview.Content = asset.TxtContent;
+
+        }
+    }
+}
